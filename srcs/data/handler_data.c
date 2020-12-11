71c47703ea9aeebc592289843147441baccb95b3
Lorenzo Amar <lorenzoamar@iMac.local>
2020-12-11 02:54:50 +0100

    list

--- srcs/data/handler_data.c
+++ srcs/data/handler_data.c
@@ -6,7 +6,7 @@
 /*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 20:48:58 by loamar            #+#    #+#             */
-/*   Updated: 2020/12/11 02:48:49 by loamar           ###   ########.fr       */
+/*   Updated: 2020/12/11 02:54:48 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,11 @@
 
 static int 	ft_size_data(t_msh *msh, char *buf)
 {
-	// int 	test; // pour le test ( a ne pas supp)
-	//
-	// test = 1; // pour le test ( a ne pas supp)
 	msh->data->size_data = 0;
 	ft_memset(buf, 0, ft_strlen(buf));
 	free(buf);
 	while (msh->data->prompt_data[msh->data->size_data] != NULL)
-	{
-		// printf("===== %d =======\n", test);
-		// printf("-> %s\n", msh->data->prompt_data[msh->data->size_data]);
-		// printf("================\n\n");
 		msh->data->size_data++;
-		// test++;
-	}
 	return (1);
 }
 
