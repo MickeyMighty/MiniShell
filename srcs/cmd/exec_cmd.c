3838a1ebff66e1ad6e6ad168824379a58ae5a323
Lorenzo Amar <lorenzoamar@iMac.local>
2021-01-11 14:47:26 +0100

    exec cmd

--- srcs/cmd/exec_cmd.c
+++ srcs/cmd/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/06 18:07:43 by loamar            #+#    #+#             */
-/*   Updated: 2021/01/11 14:47:03 by loamar           ###   ########.fr       */
+/*   Updated: 2021/01/11 14:47:24 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int 	exec_cmd(t_msh *msh, t_list *cmd)
 	int 	count;
 	char 	*exec_path;
 
-	printf("exec\n");
 	count = 0;
 	exec_path = NULL;
 	msh->utils->pid = fork();
