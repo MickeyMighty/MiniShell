be22f93a256e0072931be6fc25c2f4731ef21704
Lorenzo Amar <lorenzoamar@iMac.local>
2021-01-11 16:16:13 +0100

    exec cmd

--- srcs/cmd/exec_cmd.c
+++ srcs/cmd/exec_cmd.c
@@ -6,16 +6,36 @@
 /*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/06 18:07:43 by loamar            #+#    #+#             */
-/*   Updated: 2021/01/11 15:47:04 by loamar           ###   ########.fr       */
+/*   Updated: 2021/01/11 16:15:57 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/libshell.h"
 
+static char    **ft_exec_args(char *cmd_path, char *arg)
+{
+    char **cmd_args;
+    int i;
+
+    i = 0;
+    if (!(cmd_args = malloc(sizeof(char *) * 3)))
+        return (NULL);
+    while (++i < 3)
+    {
+        cmd_args[0] = cmd_path;
+        cmd_args[1] = arg;
+        if (arg == NULL)
+            break;
+        cmd_args[2] = NULL;
+    }
+    return (cmd_args);
+}
+
 int 	exec_cmd(t_msh *msh, t_list *cmd, char **env)
 {
 	int 	count;
 	char 	*exec_path;
+	char	**exec_args;
 
 	count = -1;
 	exec_path = NULL;
@@ -29,8 +49,9 @@ int 	exec_cmd(t_msh *msh, t_list *cmd, char **env)
 			// printf("cmd->content = %s\n", cmd->content);
 			exec_path = ft_strjoin(msh->utils->path[count], "/");
 			exec_path = ft_strjoin(exec_path, cmd->content);
-			exec_path = ft_strjoin(exec_path, NULL);
-			execve(exec_path, NULL, env);
+			exec_args = ft_exec_args(exec_path, NULL);
+			// printf("exec_path => %s\n", exec_path);
+			execve(exec_path, exec_args, env);
 		}
 	}
 	else
