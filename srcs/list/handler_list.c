b265cd3be64bce8d4c94b93022e8fa6ee7b90e3a
Loamar <loamar@e3r10p14.42.fr>
2021-01-19 17:12:11 +0100

    handler args

--- srcs/list/handler_list.c
+++ srcs/list/handler_list.c
@@ -6,7 +6,7 @@
 /*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/11 04:42:10 by loamar            #+#    #+#             */
-/*   Updated: 2021/01/19 16:24:27 by loamar           ###   ########.fr       */
+/*   Updated: 2021/01/19 17:02:09 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ static 		int 	pop_list(t_msh *msh, int pos)
 	i = 1;
   	if(msh->lair_list->size == 0)
     	return -1;
-	printf("poet\n");
 	if(pos == msh->lair_list->size)
 	{ /* suppression du dernier élément */
     	delete_element = msh->lair_list->end;
@@ -30,26 +29,19 @@ static 		int 	pop_list(t_msh *msh, int pos)
   	}
 	else
 	{ /* suppression ailleurs */
-		printf("poet 2\n");
     	now_element = msh->lair_list->start;
 		while (i < pos)
 		{
-			printf("poet 3\n");
 			now_element = now_element->next;
 			i++;
 		}
     	delete_element = now_element;
-		printf("poet 4\n");
     	now_element->previous->next = now_element->next;
 		now_element->next->previous = now_element->previous;
-		printf("poet 44\n");
-		printf("poet 444\n");
   	}
-	printf("poet 5\n");
   	free(delete_element->content);
   	free(delete_element);
   	msh->lair_list->size--;
-	printf("poet 6\n");
   	return (0);
 }
 
@@ -68,7 +60,7 @@ static void 	ft_put_args(t_msh *msh, t_list *cmd)
 		cmd = cmd->next;
 		msh->utils->size_opt_arg++;
 	}
-	printf("msh->utils->size_opt_arg = %d\n", msh->utils->size_opt_arg);
+	// printf("msh->utils->size_opt_arg = %d\n", msh->utils->size_opt_arg);
 	cmd = tmp;
 	if (!(cmd->tab_args = malloc(sizeof(char *) * (msh->utils->size_opt_arg + 2))))
 		handler_error(msh);
@@ -91,13 +83,10 @@ static void    create_tab_args(t_msh *msh)
 	cmd = msh->lair_list->start;
 	if (cmd->token == CMD)
 	{
-		printf("ok\n");
 		while (cmd != NULL)
 		{
-			printf("ok2\n");
 			ft_put_args(msh, cmd);
 			// cmd = ft_put_args(msh, cmd);
-			printf("ok3\n");
 			cmd = cmd->next;
 			if (cmd == NULL)
 				break;
@@ -105,29 +94,23 @@ static void    create_tab_args(t_msh *msh)
 			while (cmd != NULL && (cmd->token == OPTION || cmd->token == ARGS))
 			{
 				tmp = cmd->next;
-				printf("tmp avant pop list -> %s\n", tmp->content);
-				printf("cmd avant pop list -> %s\n", cmd->content);
-				printf("ok4\n");
-				printf("size list = %d\n", msh->lair_list->size);
+				// printf("tmp avant pop list -> %s\n", tmp->content);
+				// printf("cmd avant pop list -> %s\n", cmd->content);
+				// printf("size list = %d\n", msh->lair_list->size);
 				pop_list(msh, msh->utils->pos_list);
 				msh->utils->pos_list++;
-				printf("ntm\n");
 				cmd = tmp;
-				printf("tmp apres pop list -> %s\n", tmp->content);
-				printf("tmp apres pop list -> %s\n", tmp->previous->content);
-				printf("cmd apres pop list -> %s\n", cmd->previous->content);
-				printf("tab[0] de cmd -> %s\n", cmd->previous->tab_args[0]);
-				printf("tab[1] de cmd -> %s\n", cmd->previous->tab_args[1]);
-				printf("tab[2] de cmd -> %s\n", cmd->previous->tab_args[2]);
-				printf("ok5\n");
-				// exit(0);
+				// printf("tmp apres pop list -> %s\n", tmp->content);
+				// printf("tmp apres pop list -> %s\n", tmp->previous->content);
+				// printf("cmd apres pop list -> %s\n", cmd->previous->content);
+				// printf("tab[0] de cmd -> %s\n", cmd->previous->tab_args[0]);
+				// printf("tab[1] de cmd -> %s\n", cmd->previous->tab_args[1]);
+				// printf("tab[2] de cmd -> %s\n", cmd->previous->tab_args[2]);
 			}
 			if (cmd->token == SEPARATOR)
 				cmd = cmd->next;
-			printf("ok5\n");
 		}
 	}
-	printf("ok6\n");
 }
 
 static void 	print_list(t_lair_list *lair_list)
@@ -141,31 +124,20 @@ static void 	print_list(t_lair_list *lair_list)
 
 	current = lair_list->start;
 	printf("| DEBUT | \n");
-	while (current != NULL)
+	while (current->content != NULL)
 	{
-		printf(" -test 0- ");
 		printf("%d-> %s\n", test, current->content);
-		printf(" -test 0.5- ");
 		if (current->tab_args != NULL)
 		{
-			printf(" -test 1- ");
 			while (current->tab_args[test2] != NULL)
 			{
 				printf("(%d)[%s]\n", test2, current->tab_args[test2]);
-				printf(" -test 2- ");
 				test2++;
 			}
 		}
-		printf(" -test 3- ");
-		if (current->next != NULL)
-		{
-			printf("\ncurrent = %s\n", current->content);
-			printf("\ncurrent->next = %s\n", current->next->content);
+		if (current->content != NULL)
 			current = current->next;
-		}
-		printf(" -test 4- ");
 		test++; // pour le printf
-		printf(" -test 5- ");
 	}
 	printf("| FIN |\n");
 	printf("size lair_list -> %d\n", lair_list->size);
