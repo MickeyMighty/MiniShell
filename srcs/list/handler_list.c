50d623e91cd5cab8397719c0b0f325dc51f30cb3
Lorenzo Amar <lorenzoamar@iMac.local>
2021-04-09 19:10:03 +0200

    maj

--- srcs/list/handler_list.c
+++ srcs/list/handler_list.c
@@ -6,7 +6,7 @@
 /*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/11 04:42:10 by loamar            #+#    #+#             */
-/*   Updated: 2021/04/09 19:09:48 by loamar           ###   ########.fr       */
+/*   Updated: 2021/04/09 19:10:01 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ int				handler_list(t_msh *msh)
 		ft_fill_end_list(msh->lair_list, msh->data->prompt_data[count]);
 		count++;
 	}
-	free(msh->data->prompt_data[msh->data->size_data])
+	free(msh->data->prompt_data[msh->data->size_data]);
 	set_token_list(msh);
 	msh->utils->pos_list = 1;
 	if (create_tab_args(msh) == ERROR)
