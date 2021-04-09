5685347962ab84b9d8230c80198577a1f1a70d7a
Lorenzo Amar <lorenzoamar@iMac.local>
2021-04-10 00:29:51 +0200

    maj

--- srcs/data/handler_data.c
+++ srcs/data/handler_data.c
@@ -6,7 +6,7 @@
 /*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/06 20:48:58 by loamar            #+#    #+#             */
-/*   Updated: 2021/04/10 00:26:56 by loamar           ###   ########.fr       */
+/*   Updated: 2021/04/10 00:29:49 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,6 @@ char			**ft_split_data(t_msh *msh, char *s, char c)
 		free(res);
 		return (split_error(split_data, ERROR_MULTI, 1));
 	}
-	res[split_data->nb_word] = 0;
 	free(split_data);
 	return (res);
 }
