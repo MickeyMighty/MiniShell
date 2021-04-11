793a8dbafbcb627bf37dc17dd6d6c1f06528c2ac
Lorenzo Amar <lorenzoamar@iMac.local>
2021-04-11 22:59:32 +0200

    maj

---
+++ test/split_data/split_data.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split_data.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/11 19:36:18 by loamar            #+#    #+#             */
+/*   Updated: 2021/04/11 12:30:34 by loamar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../test.h"
+
+int				ft_size_quote(t_split_data *split_data, char *str, int index)
+{
+	char	quote;
+
+	quote = str[index];
+	index++;
+	while (str[index])
+	{
+		if (str[index] == quote && quote == SQUOTE)
+			return (index);
+		if (str[index] == quote && str[index - 1] != '\\' && quote == DQUOTE)
+			return (index);
+		index++;
+	}
+	split_data->error = 1;
+	return (index);
+}
+
+char			**ft_word_to_tab(char *str,
+t_split_data *split_data, char **res)
+{
+	int		i;
+	int		loop;
+
+	i = 0;
+	loop = 0;
+	split_data->nb = 0;
+	split_data->error = 0;
+	while (str[split_data->pos] && (split_data->nb < split_data->nb_word))
+	{
+		loop++;
+		split_data->pos_index = 0;
+		while (str[split_data->pos] != '\0' && str[split_data->pos] == ' ')
+			split_data->pos++;
+		res[split_data->nb] = ft_substr(str, split_data->pos,
+		ft_get_len_word(split_data, str));
+		split_data->nb++;
+		split_data->pos = split_data->pos_index;
+		if (res[split_data->nb - 1] == NULL && split_data->error == 1)
+		{
+			while (i < split_data->nb_word && i < loop)
+			{
+				free(res[i]);
+				i++;
+			}
+			return (NULL);
+		}
+	}
+	return (res);
+}
+
+static	int		get_pos_after_word(char *s, char c,
+t_split_data *split_data)
+{
+	split_data->nb_word++;
+	if (s[split_data->pos] == '\\' && s[split_data->pos + 1] == c)
+		split_data->pos += 2;
+	while (s[split_data->pos] != '\0' && s[split_data->pos] != c
+	&& (ft_count_separator(split_data, s, split_data->pos) == 0))
+	{
+		if ((split_data->pos = check_word_qte(split_data, s)) == ERROR)
+			return (-1);
+		else if (s[split_data->pos] == '\\' && s[split_data->pos + 1])
+			split_data->pos++;
+		if (s[split_data->pos])
+			split_data->pos++;
+	}
+	return (1);
+}
+
+void			ft_count_word(char *s, char c,
+t_split_data *split_data)
+{
+	while (s[split_data->pos])
+	{
+		while (s[split_data->pos] == c)
+			split_data->pos++;
+		if (s[split_data->pos] != '\0' && s[split_data->pos] != c
+		&& (ft_count_separator(split_data, s, split_data->pos) == 0))
+			if (get_pos_after_word(s, c, split_data) == -1)
+				return ;
+		split_data->double_semicolon = 0;
+		if (s[split_data->pos] && ft_count_separator(split_data, s,
+			split_data->pos) != 0)
+		{
+			split_data->nb_word++;
+			if (split_data->double_semicolon == 1 && s[split_data->pos + 1])
+				if (ft_count_separator(split_data, s, (split_data->pos + 1))
+				== 1)
+					if (split_data->double_semicolon == 2)
+						split_data->error = 2;
+			split_data->pos += ft_count_separator(split_data, s,
+				split_data->pos);
+		}
+		else if (s[split_data->pos] != '\0')
+			split_data->pos++;
+	}
+}
---
+++ test/split_data/split_data_utils.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split_data_utils.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/11 19:43:38 by loamar            #+#    #+#             */
+/*   Updated: 2021/04/11 12:30:20 by loamar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../test.h"
+
+int				check_word(char *s, char c)
+{
+	int		pos;
+
+	pos = 0;
+	while (s[pos] == c)
+		pos++;
+	if (s[pos] == '\0')
+		return (1);
+	else
+		return (0);
+}
+
+t_split_data	*init_split_data(t_split_data *split_data)
+{
+	if (!(split_data = (t_split_data*)malloc(sizeof(t_split_data))))
+		return (NULL);
+	split_data->error = 0;
+	split_data->size = 0;
+	split_data->double_semicolon = 0;
+	split_data->nb_word = 0;
+	split_data->nb = 0;
+	split_data->pos_index = 0;
+	split_data->pos = 0;
+	return (split_data);
+}
+
+int				ft_count_separator(t_split_data *split_data, char *s, int pos)
+{
+	if (s[pos + 1])
+		if ((s[pos] == '>' && s[pos + 1] == '>'))
+			return (2);
+	if (s[pos] == ';' || s[pos] == '|' || s[pos] == '<' || s[pos] == '>')
+	{
+		if (split_data->double_semicolon == 0)
+			split_data->double_semicolon = 1;
+		else if (split_data->double_semicolon == 1)
+			split_data->double_semicolon = 2;
+		return (1);
+	}
+	return (0);
+}
+
+static	int		check_word_squote(t_split_data *split_data, char *str,
+int index)
+{
+	index++;
+	while (str[index] && str[index] != SQUOTE)
+		index++;
+	if (str[index] == SQUOTE)
+		return (index);
+	split_data->error = 1;
+	return (index);
+}
+
+int				check_word_qte(t_split_data *split_data, char *str)
+{
+	int		index;
+
+	index = split_data->pos;
+	if (str[index] == SQUOTE)
+		return (check_word_squote(split_data, str, index));
+	else if (str[index] == DQUOTE)
+	{
+		index = get_split_pos(str, split_data, index);
+		if (index == -1)
+			return (ERROR);
+		if (str[index] == DQUOTE)
+			return (index);
+		else
+		{
+			split_data->error = 1;
+			return (ERROR);
+		}
+	}
+	else if ((index == (int)ft_strlen(str)) && str[index - 1] == '\\')
+	{
+		split_data->error = 1;
+		return (ERROR);
+	}
+	return (index);
+}
