22feecb227eb51afd366912a97ddac08f8c33bad
Lorenzo Amar <lorenzoamar@iMac.local>
2021-04-13 15:03:24 +0200

    maj

--- lib/get_next_line/get_next_line.c
+++ lib/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 00:37:54 by loamar            #+#    #+#             */
-/*   Updated: 2021/04/13 15:00:38 by loamar           ###   ########.fr       */
+/*   Updated: 2021/04/13 15:03:21 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,8 @@ static int		ft_read(int fd, char *buf, char **str, char **line)
 		return (ft_free(str, -1));
 	if (bytes)
 		return (1);
+	if (*line == "")
+		free(*line);
 	while ((bytes = read(fd, buf, BUFFER_SIZE)) >= 0)
 	{
 		buf[bytes] = '\0';
