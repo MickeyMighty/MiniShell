36177d8c22757e95a240764eef6434bb529a205f
Lorenzo Amar <lorenzoamar@iMac.local>
2021-04-14 13:15:51 +0200

    maj

--- srcs/cmd/check/return_check.c
+++ srcs/cmd/check/return_check.c
@@ -6,7 +6,7 @@
 /*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/13 15:13:10 by loamar            #+#    #+#             */
-/*   Updated: 2021/04/13 16:11:44 by loamar           ###   ########.fr       */
+/*   Updated: 2021/04/14 13:15:49 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -123,6 +123,7 @@ char				*return_all_content(t_msh *msh, char *str)
 		if (str[msh->utils->pos])
 			msh->utils->pos++;
 	}
+	printf("first_step = [%s]\n", first_step);
 	free(str);
 	return (first_step);
 }
