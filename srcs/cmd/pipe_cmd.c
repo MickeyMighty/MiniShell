da4d14a5decff7d938c21e10f740e35f35c3a7ea
Lorenzo Amar <lorenzoamar@iMac.local>
2021-04-15 13:11:07 +0200

    maj

--- srcs/cmd/pipe_cmd.c
+++ srcs/cmd/pipe_cmd.c
@@ -6,7 +6,7 @@
 /*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 07:42:36 by loamar            #+#    #+#             */
-/*   Updated: 2021/04/15 12:57:40 by loamar           ###   ########.fr       */
+/*   Updated: 2021/04/15 13:10:49 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,7 @@ t_list			*multi_pipe(t_msh *msh, t_list *element, char **env)
 			return (element);
 		}
 	}
+	free_all(msh, EXIT);
 	close(msh->utils->backup_fd);
 	return (element);
 }
