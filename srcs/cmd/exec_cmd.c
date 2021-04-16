854a0fe63e806cb2154318a058281ce4427906b5
Lorenzo Amar <lorenzoamar@iMac.local>
2021-04-16 11:42:55 +0200

    maj

--- srcs/cmd/exec_cmd.c
+++ srcs/cmd/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 22:57:42 by loamar            #+#    #+#             */
-/*   Updated: 2021/04/16 11:40:46 by loamar           ###   ########.fr       */
+/*   Updated: 2021/04/16 11:42:53 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,14 +63,7 @@ int					exec_cmd(t_msh *msh, t_list *cmd, char **env, int pipe)
 	msh->utils->pipe = pipe;
 	status = ft_handler_builtins(msh, cmd);
 	if (status == SUCCESS)
-	{
-		if (pipe == 1)
-		{
-			printf("free\n");
-			free_all(msh, EXIT);
-		}
 		g_status = status;
-	}
 	else if (status == ERROR_BUILTINS)
 		if (check_permission_exec(msh, cmd, env) == ERROR)
 			return (ERROR);
