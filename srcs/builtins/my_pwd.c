44862d8e800f5ce0a931808d2f8fb8e552c0c83c
Lorenzo Amar <lorenzoamar@iMac.local>
2021-04-16 11:11:34 +0200

    maj

--- srcs/builtins/my_pwd.c
+++ srcs/builtins/my_pwd.c
@@ -6,7 +6,7 @@
 /*   By: tidminta <tidminta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 16:01:14 by tidminta          #+#    #+#             */
-/*   Updated: 2021/04/16 11:10:47 by loamar           ###   ########.fr       */
+/*   Updated: 2021/04/16 11:11:32 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,5 @@ int			my_pwd(void)
 	ft_putendl_fd(pwd, 1);
 	free(pwd);
 	pwd = NULL;
-	if (msh->utils->pipe == 1)
-		free_all(msh, EXIT);
 	return (SUCCESS);
 }
