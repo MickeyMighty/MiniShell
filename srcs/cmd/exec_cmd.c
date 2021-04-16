a3e0334937e7c27cb113c5d6c25dc5a7d41eebb7
Lorenzo Amar <lorenzoamar@iMac.local>
2021-04-16 11:22:04 +0200

    maj

--- srcs/cmd/exec_cmd.c
+++ srcs/cmd/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: loamar <loamar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 22:57:42 by loamar            #+#    #+#             */
-/*   Updated: 2021/04/16 11:17:16 by loamar           ###   ########.fr       */
+/*   Updated: 2021/04/16 11:21:55 by loamar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,8 +74,8 @@ int					exec_cmd(t_msh *msh, t_list *cmd, char **env, int pipe)
 		// free(exec_path);
 		if (msh->utils->pipe == 1)
 		{
+			g_loop = ENDLOOP;
 			free_all(msh, EXIT);
-			exit(status);
 		}
 	}
 	else if (status == ERROR_BUILTINS)
